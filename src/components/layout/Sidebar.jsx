2ec9e31c2b502d41e5d22e41c1b0e28b72c93433
Keith Kamau <keith.kamau2@student.moringaschool.com>
2026-05-23 10:31:41 +0300

    patch: removal of estate map feat

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -58,23 +58,6 @@ const adminItems = [
     ),
   },
   {
-    label: "Estate Map",
-    page: "map",
-    icon: (
-      <svg
-        className='w-5 h-5'
-        fill='none'
-        stroke='currentColor'
-        strokeWidth='2'
-        viewBox='0 0 24 24'
-      >
-        <polygon points='1 6 1 22 8 18 16 22 23 18 23 2 16 6 8 2 1 6' />
-        <line x1='8' y1='2' x2='8' y2='18' />
-        <line x1='16' y1='6' x2='16' y2='22' />
-      </svg>
-    ),
-  },
-  {
     label: "Residents",
     page: "residents",
     icon: (
